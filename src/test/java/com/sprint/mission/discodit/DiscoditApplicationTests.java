e051166e5152d2553af99b6697b31bd16974a1f9
김지성 <shes3332@naver.com>
2026-03-11 17:09:31 +0900

    Spring 프로젝트 초기 설정

---
+++ src/test/java/com/sprint/mission/discodit/DiscoditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint.mission.discodit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
