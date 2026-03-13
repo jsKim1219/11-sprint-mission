76dc20a84afbcafda130d8c21565de1e17849203
김지성 <shes3332@naver.com>
2026-03-13 19:55:30 +0900

    refactor: Spring Context를 활용하여 Service 초기화 로직 변경

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -9,6 +9,7 @@ import com.sprint.mission.discodeit.service.MessageService;
 import com.sprint.mission.discodeit.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DiscodeitApplication {
@@ -29,6 +30,15 @@ public class DiscodeitApplication {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiscodeitApplication.class, args);
+		ConfigurableApplicationContext context =
+				SpringApplication.run(DiscodeitApplication.class, args);
+
+		UserService userService = context.getBean(UserService.class);
+		ChannelService channelService = context.getBean(ChannelService.class);
+		MessageService messageService = context.getBean(MessageService.class);
+
+		User user = setupUser(userService);
+		Channel channel = setupChannel(channelService);
+		messageCreateTest(messageService, channel, user);
 	}
 }
