7a69213f1c41bf46d3042cc93e6db536b9596b7e
김지성 <shes3332@naver.com>
2026-03-24 16:31:59 +0900

    feat: 사용자 관리 클래스 구현

---
+++ src/main/java/com/sprint/mission/discodeit/controller/UserController.java
@@ -0,0 +1,48 @@
+package com.sprint.mission.discodeit.controller;
+
+import com.sprint.mission.discodeit.dto.UserCreateRequest;
+import com.sprint.mission.discodeit.dto.UserDto;
+import com.sprint.mission.discodeit.dto.UserStatusUpdateRequest;
+import com.sprint.mission.discodeit.dto.UserUpdateRequst;
+import com.sprint.mission.discodeit.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public UserDto registerUser(@RequestBody UserCreateRequest request) {
+        return userService.create(request);
+    }
+
+    @RequestMapping(value = "/{userId}", method = RequestMethod.PUT)
+    public void updateUser(
+            @PathVariable UUID userId, @RequestBody UserUpdateRequst requst) {
+        userService.update(userId, requst);
+    }
+
+    @RequestMapping(value = "/{userId}", method = RequestMethod.DELETE)
+    public void  deleteUser(@PathVariable UUID userId) {
+        userService.delete(userId);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<UserDto> getAllUsers() {
+        return userService.findAll();
+    }
+
+    @RequestMapping(value = "/{userId}/online-status", method = RequestMethod.PUT)
+    public void updateOnlineStatus(
+            @PathVariable UUID userId, @RequestBody UserStatusUpdateRequest request) {
+        //
+    }
+}
