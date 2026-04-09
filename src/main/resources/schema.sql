4494b0bcf07a11ad159a1267d276e9068f09060f
김지성 <shes3332@naver.com>
2026-04-09 16:52:19 +0900

    feat: 테이블 생성

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE binary_contents
+(
+    id           UUID PRIMARY KEY,
+    created_at   TIMESTAMP WITH TIME ZONE NOT NULL,
+    file_name    VARCHAR(255)             NOT NULL,
+    size         BIGINT                   NOT NULL,
+    content_type VARCHAR(100)             NOT NULL,
+    bytes        BYTEA                    NOT NULL
+);
+
+CREATE TABLE channels
+(
+    id          UUID PRIMARY KEY,
+    created_at  TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at  TIMESTAMP WITH TIME ZONE,
+    name        VARCHAR(100),
+    description VARCHAR(500),
+    type        VARCHAR(10)              NOT NULL CHECK ( type IN ('PUBLIC', 'PRIVATE'))
+);
