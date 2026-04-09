24f9e1adee557bcf896c968d88b3fdb359581068
김지성 <shes3332@naver.com>
2026-04-09 19:03:01 +0900

    feat: schema.sql 테이블 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,30 +1,75 @@
-CREATE TABLE binary_contents
-(
-    id           UUID PRIMARY KEY,
-    created_at   TIMESTAMP WITH TIME ZONE NOT NULL,
-    file_name    VARCHAR(255)             NOT NULL,
-    size         BIGINT                   NOT NULL,
-    content_type VARCHAR(100)             NOT NULL,
-    bytes        BYTEA                    NOT NULL
-);
-
-CREATE TABLE channels
-(
-    id          UUID PRIMARY KEY,
-    created_at  TIMESTAMP WITH TIME ZONE NOT NULL,
-    updated_at  TIMESTAMP WITH TIME ZONE,
-    name        VARCHAR(100),
-    description VARCHAR(500),
-    type        VARCHAR(10)              NOT NULL CHECK ( type IN ('PUBLIC', 'PRIVATE'))
-);
-
-CREATE TABLE users
+CREATE TABLE IF NOT EXISTS users
 (
     id         UUID PRIMARY KEY,
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-    updated_at TIMESTAMP WITH TIME ZONE,
-    username   VARCHAR(50) UNIQUE       NOT NULL,
-    email      VARCHAR(100) UNIQUE      NOT NULL,
-    password   VARCHAR(60)              NOT NULL,
-    profile_id UUID UNIQUE              REFERENCES binary_contents (id) ON DELETE SET NULL
+    created_at TIMESTAMPTZ         NOT NULL,
+    updated_at TIMESTAMPTZ,
+    username   VARCHAR(50) UNIQUE  NOT NULL,
+    email      VARCHAR(100) UNIQUE NOT NULL,
+    password   VARCHAR(60)         NOT NULL,
+    profile_id UUID UNIQUE,
+    FOREIGN KEY (profile_id) REFERENCES binary_contents (id) ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS channels
+(
+    id          UUID PRIMARY KEY,
+    created_at  TIMESTAMPTZ NOT NULL,
+    updated_at  TIMESTAMPTZ,
+    name        VARCHAR(100),
+    description VARCHAR(500),
+    type        VARCHAR(10) NOT NULL CHECK ( type IN ('PUBLIC', 'PRIVATE'))
+);
+
+CREATE TABLE IF NOT EXISTS messages
+(
+    id         UUID PRIMARY KEY,
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ,
+    content    TEXT,
+    channel_id UUID        NOT NULL,
+    author_id  UUID,
+    FOREIGN KEY (channel_id) REFERENCES channels (id) ON DELETE CASCADE,
+    FOREIGN KEY (author_id) REFERENCES users (id) ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS user_status
+(
+    id             UUID PRIMARY KEY,
+    created_at     TIMESTAMPTZ NOT NULL,
+    updated_at     TIMESTAMPTZ,
+    user_id        UUID        NOT NULL UNIQUE,
+    last_active_at TIMESTAMPTZ NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS read_statuses
+(
+    id           UUID PRIMARY KEY,
+    created_at   TIMESTAMPTZ NOT NULL,
+    updated_at   TIMESTAMPTZ,
+    user_id      UUID        NOT NULL,
+    channel_id   UUID        NOT NULL,
+    last_read_at TIMESTAMPTZ NOT NULL,
+    UNIQUE (user_id, channel_id),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (channel_id) REFERENCES channels (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS message_attachments
+(
+    message_id    UUID NOT NULL,
+    attachment_id UUID NOT NULL,
+    PRIMARY KEY (message_id, attachment_id),
+    FOREIGN KEY (message_id) REFERENCES messages (id) ON DELETE CASCADE,
+    FOREIGN KEY (attachment_id) REFERENCES binary_contents (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS binary_contents
+(
+    id           UUID PRIMARY KEY,
+    created_at   TIMESTAMPTZ  NOT NULL,
+    file_name    VARCHAR(255) NOT NULL,
+    size         BIGINT       NOT NULL,
+    content_type VARCHAR(100) NOT NULL,
+    bytes        BYTEA        NOT NULL
 );
