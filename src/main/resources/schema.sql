915c089a23576f5ff5b17426b528f605254fcfba
김지성 <shes3332@naver.com>
2026-04-09 17:36:05 +0900

    feat: Spring Data JPA 적용

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -17,3 +17,14 @@ CREATE TABLE channels
     description VARCHAR(500),
     type        VARCHAR(10)              NOT NULL CHECK ( type IN ('PUBLIC', 'PRIVATE'))
 );
+
+CREATE TABLE users
+(
+    id         UUID PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE,
+    username   VARCHAR(50) UNIQUE       NOT NULL,
+    email      VARCHAR(100) UNIQUE      NOT NULL,
+    password   VARCHAR(60)              NOT NULL,
+    profile_id UUID UNIQUE              REFERENCES binary_contents (id) ON DELETE SET NULL
+);
